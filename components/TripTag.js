fd2841648606d2d26538fe4f7f63c25517a0f6fe
niuniuanran <irene.anran.niu@gmail.com>
2020-03-21 22:38:28 +1300

    add side bar tag component

---
+++ components/TripTag.js
@@ -0,0 +1,26 @@
+/** @format */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import style from '../css/trip-tag.module.css';
+
+export default function TripTag({ tagName, isActive }) {
+  const backgroundImageStyle = {
+    backgroundImage: `linear-gradient(to right, rgba(255,255,255,0.8), rgba(255,255,255,0.8)), 
+    url('/tag-img/${tagName.toLowerCase()}-tag.jpg')`,
+  };
+
+  return (
+    <div
+      className={isActive ? style.active : style.tag}
+      style={backgroundImageStyle}
+    >
+      <span className={style.name}>{tagName}</span>
+    </div>
+  );
+}
+
+TripTag.propTypes = {
+  tagName: PropTypes.string.isRequired,
+  isActive: PropTypes.bool,
+};
