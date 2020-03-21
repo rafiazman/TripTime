f6508f67d8800fae231d99e818129c8b482056a4
niuniuanran <irene.anran.niu@gmail.com>
2020-03-21 18:04:27 +1300

    ran prettier

--- components/TripLink.js
+++ components/TripLink.js
@@ -1,22 +1,24 @@
-import React from 'react'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
+/** @format */
+
+import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import PropTypes from 'prop-types';
 
-function TripLink({ href, children }){
+function TripLink({ href, children }) {
   const router = useRouter();
 
   let className = children.props.className || '';
   if (router.pathname === href) {
-    className = `${className} selected`
+    className = `${className} selected`;
   }
 
-  return <Link href={href}>{React.cloneElement(children, { className })}</Link>
+  return <Link href={href}>{React.cloneElement(children, { className })}</Link>;
 }
 
 TripLink.propTypes = {
   children: PropTypes.node.isRequired,
-  href: PropTypes.string.isRequired
+  href: PropTypes.string.isRequired,
 };
 
 export default TripLink;
