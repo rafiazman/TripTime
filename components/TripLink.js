bb0923c9baad06cea00b94d3a8ce542d2dbc0014
niuniuanran <irene.anran.niu@gmail.com>
2020-03-21 22:37:54 +1300

    add nav link component that wraps next/link Link

---
+++ components/TripLink.js
@@ -0,0 +1,31 @@
+/** @format */
+
+import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import PropTypes from 'prop-types';
+import TripTag from './TripTag';
+
+const tripAnchorStyle = {
+  color: 'inherit',
+  textDecoration: 'inherit',
+};
+
+function TripLink({ linkName }) {
+  const router = useRouter();
+  const href = linkName.toLowerCase();
+
+  return (
+    <Link href={href}>
+      <a style={tripAnchorStyle}>
+        <TripTag tagName={linkName} isActive={router.pathname === href} />
+      </a>
+    </Link>
+  );
+}
+
+TripLink.propTypes = {
+  linkName: PropTypes.string.isRequired,
+};
+
+export default TripLink;
