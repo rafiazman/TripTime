1ddec8014e902795632e718995cd7ac81783a72d
Rafi Azman <rafi.azman.7@gmail.com>
2020-03-23 10:43:21 +1300

    Add NPM packages required for feature
    axios added to perform HTTP requests in test,
    bcryptjs for hashing passwords
    dotenv for reading environment vars from .env file
    mongoose for connecting to MongoDB
    validator for validating user input
    babel for enabling import statements to be used in Jest
    test-listen for getting the URL of test server

---
+++ next.config.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+// Get a collection of all variables that were defined in process.env and in the
+// .env file, except for disallowed variables.
+// See https://err.sh/zeit/next.js/env-key-not-allowed for more details.
+function getAllAllowedEnvironmentVariables() {
+  return Object.entries(process.env).reduce(
+    (vars, [name, value]) =>
+      /^(?:__|NODE_)/.test(name) ? vars : { ...vars, [name]: value },
+    {},
+  )
+}
+
+module.exports = {
+  env: {
+    // Make all variables that were defined in process.env and the .env file available at build time.
+    ...getAllAllowedEnvironmentVariables(),
+
+    // Reference a specific variable that was defined in process.env or the .env file.
+    TEST: process.env.TEST,
+  },
+}
