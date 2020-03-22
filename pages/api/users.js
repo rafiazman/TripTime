5f174c8d84ee6751c5c414d48d582c6bc22bd3c8
Rafi Azman <rafi.azman.7@gmail.com>
2020-03-23 10:58:08 +1300

    Implement POST /api/users example
    Submitting a POST request "registers" a user by saving it to MongoDB

---
+++ pages/api/users.js
@@ -0,0 +1,52 @@
+/** @format */
+
+import isEmail from 'validator/lib/isEmail';
+import bcrypt from 'bcryptjs';
+import User from '../../app/models/User';
+import connectToDb from '../../connectToDb';
+
+const handler = async (req, res) => {
+  await connectToDb();
+
+  if (req.method === 'POST') {
+    const { email, name, password } = req.body;
+
+    if (!isEmail(email)) {
+      return res.send({
+        status: 'error',
+        message: 'The email you entered is invalid',
+      });
+    }
+
+    User.where({ email })
+      .countDocuments()
+      .then(count => {
+        if (count)
+          return Promise.reject(Error('The email has already been used'));
+
+        const hashedPwd = bcrypt.hashSync(password, 10);
+
+        const newUser = User.create({ email, name, password: hashedPwd });
+        newUser
+          .then(() => {
+            res.status(201).send({
+              status: 'ok',
+              message: 'User signed up successfully',
+            });
+          })
+          .catch(err => Promise.reject(err));
+      })
+      .catch(error => {
+        res.send({
+          status: 'error',
+          message: error.toString(),
+        });
+      });
+    return;
+  }
+
+  // Return 405 Method Not Allowed
+  return res.status(405).end();
+};
+
+export default handler;
