92d0b655f5f8d576a7e95b798c64469f4991aefb
Rafi Azman <rafi.azman.7@gmail.com>
2020-03-23 17:25:44 +1300

    Refactor test code and close Mongoose connection on finish test

--- __tests__/pages/api/users.test.js
+++ __tests__/pages/api/users.test.js
@@ -5,19 +5,26 @@ import fetch from 'isomorphic-unfetch';
 import listen from 'test-listen';
 import { apiResolver } from 'next/dist/next-server/server/api-utils';
 import handler from '../../../pages/api/users';
+import mongoose from 'mongoose';
 
 describe('/api/users handler', () => {
-  it('responds 405 Method Not Allowed to GET requests', async () => {
-    expect.assertions(1);
-    let requestHandler = (req, res) => {
-      return apiResolver(req, res, undefined, handler, undefined);
-    };
-    let server = http.createServer(requestHandler);
-    let url = await listen(server);
+  let server;
+  let url;
 
-    let response = await fetch(url);
+  beforeAll(async done => {
+    server = http.createServer((req, res) => apiResolver(req, res, undefined, handler, undefined));
+    url = await listen(server);
+    done();
+  });
+
+  afterAll(done => {
+    mongoose.connection.close();
+    server.close(done);
+  });
+
+  it('responds 405 Method Not Allowed to GET requests', async () => {
+    const response = await fetch(url);
 
     expect(response.status).toBe(405);
-    return server.close();
   });
 });
