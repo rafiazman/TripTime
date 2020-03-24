af8d34e11b3be3325af48056b5fd4b50d134cc78
niuniuanran <irene.anran.niu@gmail.com>
2020-03-24 15:03:37 +1300

    apply react-calendar on calendar page

--- pages/calendar.jsx
+++ pages/calendar.jsx
@@ -4,12 +4,10 @@ import withLayout from '../components/Layout';
 import ReactCalendar from 'react-calendar';
 import styles from '../css/calendar.module.css';
 
-
 function Calendar() {
   return (
     <div className={styles.calendarContainer}>
       <ReactCalendar />
-
     </div>
   );
 }
