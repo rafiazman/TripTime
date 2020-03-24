bc32761912b3fd41a511cbde0b239eb07901478d
niuniuanran <irene.anran.niu@gmail.com>
2020-03-24 15:02:47 +1300

    improve calendar styling

--- pages/calendar.jsx
+++ pages/calendar.jsx
@@ -1,11 +1,15 @@
 /** @format */
 import React from 'react';
 import withLayout from '../components/Layout';
+import ReactCalendar from 'react-calendar';
+import styles from '../css/calendar.module.css';
+
 
 function Calendar() {
   return (
-    <div>
-      <p>This is the calendar</p>
+    <div className={styles.calendarContainer}>
+      <ReactCalendar />
+
     </div>
   );
 }
