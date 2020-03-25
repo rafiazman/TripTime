272b6d5aeafd0f633a90af22edd6742b6e7fde58
niuniuanran <irene.anran.niu@gmail.com>
2020-03-25 21:09:38 +1300

    add calendar summary

---
+++ dummy-data/events.js
@@ -0,0 +1,42 @@
+/** @format */
+
+const events = [
+  {
+    id: '16b020e1-1214-4977-a40b-3df1b7407149',
+    type: 'outdoor',
+    start: '2020-03-25T09:00:00+13:00',
+    end: '2020-03-25T16:00:00+13:00',
+    name: 'Wind Turbine Walkway',
+    description: 'Hike through the wind turbine walkway',
+    modified: '2020-03-09T15:00:00+13:00',
+  },
+  {
+    id: '0ea1142a-eab8-4a3c-b9bd-404722915a14',
+    type: 'event',
+    start: '2020-03-26T09:00:00+13:00',
+    end: '2020-03-26T11:00:00+13:00',
+    name: 'Musical Party',
+    description: 'Musical Party at Campus',
+    modified: '2020-03-09T15:00:00+13:00',
+  },
+  {
+    id: '2172269c-2437-418a-85d5-82f8ebfa372e',
+    type: 'eating',
+    start: '2020-03-25T11:00:00+13:00',
+    end: '2020-03-25T13:00:00+13:00',
+    name: 'Lunch',
+    description: 'Lunch at 88 Restaurant',
+    modified: '2020-03-09T15:00:00+13:00',
+  },
+  {
+    id: '862bb995-12a7-46fa-bf74-1b0c35a8c9aa',
+    type: 'relax',
+    start: '2020-03-10T09:00:00+13:00',
+    end: '2020-03-10T11:00:00+13:00',
+    name: 'Hotel relax time',
+    description: 'Relax at hotel and play cards',
+    modified: '2020-03-09T15:00:00+13:00',
+  },
+];
+
+export default events;
--- pages/calendar.jsx
+++ pages/calendar.jsx
@@ -3,11 +3,14 @@ import React from 'react';
 import withLayout from '../components/Layout';
 import ReactCalendar from 'react-calendar';
 import styles from '../css/calendar.module.css';
+import CalendarSummary from '../components/CalendarSummary';
+import events from '../dummy-data/events';
 
 function Calendar() {
   return (
     <div className={styles.calendarContainer}>
       <ReactCalendar />
+      <CalendarSummary events={events} />
     </div>
   );
 }
