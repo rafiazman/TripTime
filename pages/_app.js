1d41deccf08cb487bdce33a5067c0dd135303ed5
Shakeel Khan <khanshak9@hotmail.com>
2020-04-07 19:43:09 +1200

    Add a scrollable map to the map tab
    Gets a map from openstreetmap and displays it in the map tab.

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@
 import '../css/global.css';
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../css/leaflet.css';
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
