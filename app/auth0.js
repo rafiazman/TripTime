dc2aa79e5660c546ecfde793298669279c2e1c1e
Rafi Azman <rafi.azman.7@gmail.com>
2020-04-08 22:46:49 +1200

    Allow for terminating backslash in .env values

--- app/auth0.js
+++ app/auth0.js
@@ -1,13 +1,14 @@
 /** @format */
 
 import { initAuth0 } from '@auth0/nextjs-auth0';
+import url from 'url';
 
 export default initAuth0({
   domain: process.env.AUTH0_DOMAIN,
   clientId: process.env.AUTH0_CLIENT_ID,
   clientSecret: process.env.AUTH0_CLIENT_SECRET,
   scope: `openid profile`,
-  redirectUri: process.env.APP_HOSTNAME + `/api/callback`,
+  redirectUri: url.resolve(process.env.APP_HOSTNAME, `/api/callback`),
   postLogoutRedirectUri: process.env.APP_HOSTNAME,
   session: {
     // The secret used to encrypt the cookie.
