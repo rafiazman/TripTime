d4cb2c3597a60c93d29cf6c6c5b60c52da881ddc
niuniuanran <irene.anran.niu@gmail.com>
2020-04-15 16:06:09 +1200

    Create and style join us form

---
+++ src/spa/components/landing/JoinUsForm.js
@@ -0,0 +1,22 @@
+/** @format */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from '../../css/join-form.module.css';
+
+class JoinUsForm extends React.Component {
+  render() {
+    return (
+      <form className={styles.form}>
+        <input type='email' placeholder='Enter your email' />
+        <button type='submit'>{this.props.invitation}</button>
+      </form>
+    );
+  }
+}
+
+JoinUsForm.propTypes = {
+  invitation: PropTypes.string.isRequired,
+};
+
+export default JoinUsForm;
