dd430beb657124feb219761937798c328a6ef1aa
niuniuanran <irene.anran.niu@gmail.com>
2020-04-16 12:39:01 +1200

    Populate TopBar options

--- src/spa/components/layout/TopBar.js
+++ src/spa/components/layout/TopBar.js
@@ -2,85 +2,94 @@
 import React from 'react';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
-
-const topBarImgStyle = {
-  maxWdth: '2.2rem',
-  maxHeight: '2.2rem',
-  display: 'inline-block',
-  margin: '5px',
-};
-const topBarSpanStyle = {
-  padding: '2px 5px',
-  fontSize: '1.5rem',
-};
-const topBarOptionStyle = {
-  fontSize: '1rem',
-  fontWeight: 'regular',
-  position: 'absolute',
-  right: '20px',
-  padding: '2px 5px',
-  display: 'flex',
-  width: '10rem',
-  justifyContent: 'space-between',
-};
-const headerStyle = {
-  display: 'flex',
-  flexDirection: 'row',
-  justifyContent: 'flex-start',
-  alignItems: 'stretch',
-  padding: '0 20px',
-  gridArea: 'header',
-  fontSize: 'larger',
-  lineHeight: '2em',
-  fontWeight: 'bolder',
-  backgroundColor: 'var(--page-background-color)',
-  width: '100vw',
-};
+import styles from '../../css/topbar.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 export default class TopBar extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { user: null };
+    this.state = { user: null, fold: true };
   }
 
   componentDidMount() {
     this.props.setUser(this);
   }
 
+  generateOptions(isLoggedIn) {
+    if (isLoggedIn)
+      return [
+        { name: 'My Trips', target: '/' },
+        { name: 'New Trip', target: '/' },
+        { name: 'Log Out', target: 'api/logout' },
+      ];
+    else
+      return [
+        { name: 'Sign Up', target: '/' },
+        { name: 'Log In', target: 'api/login' },
+      ];
+  }
+
   render() {
     return (
-      <header style={headerStyle}>
-        <Link href={'/'}>
-          <a>
-            <img src='/img/logo.svg' alt='' style={topBarImgStyle} />
-          </a>
-        </Link>
-        <span style={topBarSpanStyle}>TripTime!</span>
-        {this.state.user ? (
-          <span style={topBarOptionStyle}>
-            {' '}
-            <img
-              src={this.state.user.picture}
-              className={'inline-avatar'}
-              alt={''}
-              style={{ margin: '3px' }}
-            />
-            <Link href={'/profile'}>
-              <a>Profile</a>
+      <header className={styles.bar}>
+        <span className='logo-span'>
+          <Link href={'/'}>
+            <a>
+              <img src='/img/logo.svg' alt='' />
+              <span className={styles.triptime}>TripTime</span>
+            </a>
+          </Link>
+        </span>
+        <span className={styles.options}>
+          {this.state.user && (
+            <Link href='profile'>
+              <a>
+                <img
+                  src={this.state.user.picture}
+                  className={styles.avatar}
+                  alt={''}
+                />
+              </a>
             </Link>
-            <Link href={'api/logout'}>
-              <a> Log out </a>
-            </Link>
+          )}
+          <span
+            className={this.state.fold ? styles.fold : styles.activeFold}
+            onClick={() => {
+              this.clickBurger();
+            }}
+          >
+            <FontAwesomeIcon icon={faBars} />
           </span>
-        ) : (
-          <span style={topBarOptionStyle}>
-            <Link href={'api/login'}>
-              <a> Log in </a>
-            </Link>
+          {!this.state.fold && (
+            <div className={styles.dropBox}>
+              {this.generateOptions(this.state.user).map((option, index) => {
+                return (
+                  <div className={styles.dropOption} key={index}>
+                    <Link href={option.target} key={index}>
+                      <a>{option.name}</a>
+                    </Link>
+                  </div>
+                );
+              })}
+            </div>
+          )}
+          <span className={styles.full}>
+            {this.generateOptions(this.state.user).map((option, index) => {
+              return (
+                <Link href={option.target} key={index}>
+                  <a>{option.name}</a>
+                </Link>
+              );
+            })}
           </span>
-        )}
+        </span>
       </header>
     );
+  }
+
+  clickBurger() {
+    this.setState(state => ({ fold: !state.fold }));
   }
 }
 
