a9d8e7cfeccf5fd52f814dc504c33d689ac5ca01
niuniuanran <irene.anran.niu@gmail.com>
2020-04-23 17:41:57 +1200

    Change myTripInfos to only give an updated boolean

---
+++ src/spa/app/dummy-data/my-trip-infos.js
@@ -0,0 +1,14 @@
+/** @format */
+
+import trip from './trip';
+import trip1 from './trip1';
+import trip2 from './trip2';
+import trip3 from './trip3';
+
+const myTripInfos = [
+  { trip: trip, updated: true },
+  { trip: trip1, updated: false },
+  { trip: trip2, updated: true },
+  { trip: trip3, updated: false },
+];
+export default myTripInfos;
