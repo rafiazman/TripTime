b69fcffde2dbabe0538d63ebab1cb4b95ce94cae
Rafi Azman <r_a@posteo.nz>
2020-04-26 19:57:35 +1200

    Exempt login, logout and register API routes from authentication

--- src/api/app/Http/Controllers/UserController.php
+++ src/api/app/Http/Controllers/UserController.php
@@ -9,7 +9,12 @@ class UserController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth:sanctum');
+        $this->middleware('auth:sanctum',
+            ['except' => [
+                'register',
+                'login',
+                'logout'
+            ]]);
     }
 
     /**
