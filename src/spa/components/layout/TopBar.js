68705b4c0dcc860b075e31538eadbb05a11e6c1f
niuniuanran <irene.anran.niu@gmail.com>
2020-04-26 18:26:19 +1200

    User AuthContext in TopBar

--- src/spa/components/layout/TopBar.js
+++ src/spa/components/layout/TopBar.js
@@ -1,99 +1,123 @@
 /** @format */
 import React from 'react';
 import Link from 'next/link';
-import PropTypes from 'prop-types';
 import styles from '../../css/topbar.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { AuthContext } from '../../contexts/AuthContext';
+import Router from 'next/router';
+import PageLoading from '../PageLoading';
 
 export default class TopBar extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { user: null, fold: true };
+    this.state = { fold: true, loggingOut: false };
   }
 
-  componentDidMount() {
-    this.props.setUser(this);
-  }
-
-  generateOptions(isLoggedIn) {
+  generateOptions(isLoggedIn, logoutHandler) {
     if (isLoggedIn)
       return [
-        { name: 'My Trips', target: '/' },
-        { name: 'New Trip', target: '/' },
-        { name: 'Log Out', target: 'api/logout' },
+        <Link href='/' key={0}>
+          <a>My Trips</a>
+        </Link>,
+        <Link href='/' key={1}>
+          <a>New Trip</a>
+        </Link>,
+        <span
+          className='pretend-link'
+          key={2}
+          onClick={async () => {
+            this.setState(() => ({ loggingOut: true }));
+            await logoutHandler();
+            this.setState(() => ({ loggingOut: false }));
+            Router.push('/');
+          }}
+        >
+          Log out
+        </span>,
       ];
     else
       return [
-        { name: 'Sign Up', target: '/' },
-        { name: 'Log In', target: 'api/login' },
+        <Link href='signup' key={0}>
+          <a>Sign Up</a>
+        </Link>,
+        <Link href='login' key={1}>
+          <a>Log In</a>
+        </Link>,
       ];
   }
 
   render() {
     return (
-      <header className={styles.bar}>
-        <span className='logo-span'>
-          <Link href={'/'}>
-            <a>
-              <img src='/img/logo.svg' alt='' />
-              <span className={styles.triptime}>TripTime</span>
-            </a>
-          </Link>
-        </span>
-        <span className={styles.options}>
-          {this.state.user && (
-            <Link href='profile'>
-              <a>
-                <img
-                  src={this.state.user.picture}
-                  className={styles.avatar}
-                  alt={''}
-                />
-              </a>
-            </Link>
-          )}
-          <span
-            className={this.state.fold ? styles.fold : styles.activeFold}
-            onClick={() => {
-              this.clickBurger();
-            }}
-          >
-            <FontAwesomeIcon icon={faBars} />
-          </span>
-          {!this.state.fold && (
-            <div className={styles.dropBox}>
-              {this.generateOptions(this.state.user).map((option, index) => {
-                return (
-                  <div className={styles.dropOption} key={index}>
-                    <Link href={option.target} key={index}>
-                      <a>{option.name}</a>
-                    </Link>
-                  </div>
-                );
-              })}
-            </div>
-          )}
-          <span className={styles.full}>
-            {this.generateOptions(this.state.user).map((option, index) => {
-              return (
-                <Link href={option.target} key={index}>
-                  <a>{option.name}</a>
+      <>
+        {this.state.loggingOut && (
+          <PageLoading message='TripTime is logging you out. See you soon:)' />
+        )}
+        <AuthContext.Consumer>
+          {({ currentUser, logout }) => (
+            <header className={styles.bar}>
+              <span className='logo-span'>
+                <Link href={'/'}>
+                  <a>
+                    <img src='/img/logo.svg' alt='' />
+                    <span className={styles.triptime}>TripTime</span>
+                  </a>
                 </Link>
-              );
-            })}
-          </span>
-        </span>
-      </header>
+              </span>
+              <span className={styles.options}>
+                {currentUser && (
+                  <Link href='profile'>
+                    <a>
+                      <img
+                        src={currentUser.avatarPath}
+                        className={styles.avatar}
+                        alt={''}
+                      />
+                    </a>
+                  </Link>
+                )}
+                <span
+                  className={this.state.fold ? styles.fold : styles.activeFold}
+                  onClick={() => {
+                    this.clickBurger();
+                  }}
+                >
+                  <FontAwesomeIcon icon={faBars} />
+                </span>
+                {!this.state.fold && (
+                  <div className={styles.dropBox}>
+                    {this.generateOptions(currentUser, logout).map(
+                      (option, index) => {
+                        return (
+                          <div key={index} className={styles.dropOption}>
+                            {' '}
+                            {option}
+                          </div>
+                        );
+                      },
+                    )}
+                  </div>
+                )}
+                <span className={styles.full}>
+                  {this.generateOptions(currentUser, logout).map(
+                    (option, index) => {
+                      return <span key={index}>{option}</span>;
+                    },
+                  )}
+                </span>
+              </span>
+            </header>
+          )}
+        </AuthContext.Consumer>
+      </>
     );
+  }
+
+  componentDidMount() {
+    this.setState(() => ({ fold: true }));
   }
 
   clickBurger() {
     this.setState(state => ({ fold: !state.fold }));
   }
 }
-
-TopBar.propTypes = {
-  user: PropTypes.any,
-  setUser: PropTypes.func,
-};
