d9ea8cbd33849d6e9b7850b6c6063821c07f2919
niuniuanran <irene.anran.niu@gmail.com>
2020-04-30 16:58:27 +1200

    Add email validation and redirection to JoinUsForm

--- src/spa/components/landing/JoinUsForm.js
+++ src/spa/components/landing/JoinUsForm.js
@@ -2,14 +2,45 @@
 
 import React from 'react';
 import styles from '../../css/join-form.module.css';
+import { AuthContext } from '../../contexts/AuthContext';
+import Router from 'next/router';
 
 class JoinUsForm extends React.Component {
   render() {
     return (
-      <form className={styles.form}>
-        <input type='email' placeholder='Enter your email' />
-        <button type='submit'>Join Today - It&apos;s Free!</button>
-      </form>
+      <AuthContext.Consumer>
+        {({ handleUserEmail, emailOccupied }) => (
+          <form
+            className={styles.form}
+            onSubmit={e => {
+              e.preventDefault();
+              Router.push('/signup');
+            }}
+          >
+            <input
+              type='email'
+              placeholder='Enter your email'
+              onChange={handleUserEmail}
+              required={true}
+            />
+            <button
+              disabled={emailOccupied}
+              type='submit'
+              className={
+                emailOccupied ? styles.disabledLink : styles.enabledLink
+              }
+            >
+              Join Today - It&apos;s Free!
+            </button>
+
+            {emailOccupied && (
+              <div className={styles.occupyAlert}>
+                Sorry, this email address has been occupied. Try another one?
+              </div>
+            )}
+          </form>
+        )}
+      </AuthContext.Consumer>
     );
   }
 }
