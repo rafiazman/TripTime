16570571988f2894a7a1746863007157f561cdfb
niuniuanran <irene.anran.niu@gmail.com>
2020-04-30 16:59:02 +1200

    Create login UI

--- src/spa/pages/login.jsx
+++ src/spa/pages/login.jsx
@@ -1,9 +1,11 @@
 /** @format */
 import React from 'react';
 import { AuthContext } from '../contexts/AuthContext';
-import styles from '../css/login.module.css';
+import styles from '../css/auth.module.css';
 import Router from 'next/router';
 import PageLoading from '../components/PageLoading';
+import Greeting from '../components/Greeting';
+import Link from 'next/link';
 
 export default class Login extends React.Component {
   constructor(props) {
@@ -22,21 +24,35 @@ export default class Login extends React.Component {
           <PageLoading message='Welcome! TripTime is logging you in :)' />
         )}
         <AuthContext.Consumer>
-          {({ login, handleUserEmail, handleUserPassword, currentUser }) => {
+          {({ login, handleLoginEmail, handleUserPassword, currentUser }) => {
             if (currentUser) Router.push('/');
             else
               return (
                 <div className={styles.formContainer}>
+                  <Greeting name='' />
                   <form
+                    className={styles.loginForm}
                     onSubmit={async e => {
                       e.preventDefault();
                       setLoading(true);
                       login();
                     }}
                   >
-                    <input type='text' onChange={handleUserEmail} />
-                    <input type='password' onChange={handleUserPassword} />
-                    <input type='submit' value='Log in' />
+                    <label>
+                      Email <input type='email' onChange={handleLoginEmail} />
+                    </label>
+                    <label>
+                      Password
+                      <input type='password' onChange={handleUserPassword} />
+                    </label>
+                    <input
+                      type='submit'
+                      value='Log in'
+                      className={styles.loginSubmit}
+                    />
+                    <Link href='/signup'>
+                      <a className={styles.registerLink}>Create New Account</a>
+                    </Link>
                   </form>
                 </div>
               );
