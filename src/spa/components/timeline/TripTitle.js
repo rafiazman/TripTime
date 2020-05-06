bdf262518019beaa30ef4eb185b7ba555577d497
niuniuanran <irene.anran.niu@gmail.com>
2020-05-06 23:10:05 +1200

    Make list of people display consistent

--- src/spa/components/timeline/TripTitle.js
+++ src/spa/components/timeline/TripTitle.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import styles from '../../css/trip-summary.module.css';
 import PropTypes from 'prop-types';
+import PeopleList from '../PeopleList';
 
 export default class TripTitle extends React.Component {
   render() {
@@ -10,20 +11,7 @@ export default class TripTitle extends React.Component {
       <div className={styles.tripTitleContainer}>
         <h1 className={styles.tripName}>{this.props.name}</h1>
         <p>{this.props.description}</p>
-        <div className={'horizontal-list'}>
-          {this.props.people.map((person, index) => (
-            <div key={index} className={styles.summaryPerson}>
-              <div>
-                <img
-                  src={`${person.avatarPath}`}
-                  alt={''}
-                  className={'block-avatar'}
-                />
-              </div>
-              <div>{person.name}</div>
-            </div>
-          ))}
-        </div>
+        <PeopleList people={this.props.people} />
       </div>
     );
   }
