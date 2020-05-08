889413ec047175d61e2e3018bd3a424292c5347f
niuniuanran <irene.anran.niu@gmail.com>
2020-05-08 16:16:28 +1200

    Plot map with data from API

---
+++ src/spa/pages/trip/[id]/map.jsx
@@ -0,0 +1,33 @@
+/** @format */
+import React from 'react';
+import Link from 'next/link';
+import TripTeamLayout from '../../../components/layout/TripTeamLayout';
+import dynamic from 'next/dynamic';
+import { AuthContext } from '../../../contexts/AuthContext';
+import { useRouter } from 'next/router';
+
+const TripMapNoSSR = dynamic(() => import('../../../components/map/TripMap'), {
+  ssr: false,
+});
+
+export default function Map() {
+  const tripID = useRouter().query.id;
+  return (
+    <AuthContext.Consumer>
+      {({ currentUser }) => (
+        <TripTeamLayout user={currentUser} tripID={tripID}>
+          {currentUser ? (
+            <TripMapNoSSR tripID={tripID} />
+          ) : (
+            <div className={'fit-center'}>
+              <Link href={'/login'}>
+                <a> Log in </a>
+              </Link>
+              to see the map
+            </div>
+          )}
+        </TripTeamLayout>
+      )}
+    </AuthContext.Consumer>
+  );
+}
