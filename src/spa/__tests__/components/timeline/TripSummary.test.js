d351d116880b693b2d03756ef132019c77881390
niuniuanran <irene.anran.niu@gmail.com>
2020-05-09 17:01:47 +1200

    Test the log in link is properly rendered

---
+++ src/spa/__tests__/components/timeline/TripSummary.test.js
@@ -0,0 +1,24 @@
+/** @format */
+import TestRenderer from 'react-test-renderer';
+import TripSummary from '../../../components/timeline/TripSummary';
+import React from 'react';
+
+import axios from 'axios';
+
+jest.mock('axios');
+
+describe('asks the user to login before showing the trip summary', () => {
+  const errResp = {
+    status: 401,
+    response: { message: 'mocking unauthorised response' },
+  };
+  axios.get.mockImplementation(() => Promise.reject(errResp));
+  const notLogInRenderer = TestRenderer.create(<TripSummary />);
+
+  const notLogInInstance = notLogInRenderer.root;
+
+  test('Render link to log in', () => {
+    const links = notLogInInstance.findAllByProps({ href: '/login' });
+    expect(links.length).toBe(2);
+  });
+});
