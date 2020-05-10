c4ae9acb862b97c3ad4525fc99bef97117244c60
Rafi Azman <r_a@posteo.nz>
2020-05-10 17:54:35 +1200

    Create base test

--- src/api/tests/Feature/Http/Controllers/ActivityControllerTest.php
+++ src/api/tests/Feature/Http/Controllers/ActivityControllerTest.php
@@ -49,4 +49,42 @@ class ActivityControllerTest extends TestCase
             'updated' => date(DATE_RFC3339, strtotime($note->updated_at))
         ]);
     }
+
+    /** @test */
+    public function creates_notes_tied_to_an_activity()
+    {
+        $user = factory(User::class)->create();
+        $trip = factory(Trip::class)->create();
+        $location = factory(Location::class)->create([
+            'coordinates' => '100.22, 20.36'
+        ]);
+        $activity = factory(Activity::class)->create();
+        $user->activities()->attach($activity);
+
+        $response = $this->actingAs($user)
+            ->json('post', "/api/activity/$activity->id/notes", [
+                'content' => 'Test note contents here'
+            ]);
+
+        $response->assertStatus(200);
+        $response->assertJsonFragment([
+            'message' => "Successfully added note to \"$activity->name\"",
+            'note' => [
+                'author' => [
+                    'id' => $user->id,
+                    'name' => $user->name,
+                    'email' => $user->email,
+                    'avatarPath' => $user->avatar_url,
+                ],
+                'content' => 'Test note contents here',
+                'updated' => date(DATE_RFC3339, strtotime(now()->toDateTimeString()))
+            ]
+        ]);
+        $this->assertDatabaseHas('notes', [
+            'body' => 'Test note contents here',
+            'user_id' => $user->id,
+            'pointer_id' => $activity->id,
+            'pointer_type' => Activity::class
+        ]);
+    }
 }
