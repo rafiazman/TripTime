a9de44f1d477af7e0b2bfdf1d131a497b8d8d00c
niuniuanran <irene.anran.niu@gmail.com>
2020-05-11 18:45:02 +1200

    Put WIP message on calendar

--- src/spa/components/calendar/CalendarSummary.js
+++ src/spa/components/calendar/CalendarSummary.js
@@ -6,56 +6,52 @@ import moment from 'moment';
 import PropTypes from 'prop-types';
 import ActivityCard from '../cards/ActivityCard';
 
-export default class CalendarSummary extends React.Component {
-  render() {
-    const events = this.props.events;
-    const now = moment();
-    const currentEvent = events.find(
-      event => now.isAfter(event.start) && now.isBefore(event.end),
-    );
-    const futureEvents = events
-      .filter(event => now.isBefore(event.start))
-      .sort();
-    const nextEvent = futureEvents && futureEvents[0];
-    const numEventsToday = futureEvents.filter(event =>
-      now.isSame(event.start, 'day'),
-    ).length;
-    const numEventsThisWeek = futureEvents.filter(event =>
-      now.isSame(event.start, 'week'),
-    ).length;
-    const numEventsThisMonth = futureEvents.filter(event =>
-      now.isSame(event.start, 'month'),
-    ).length;
+export default function CalendarSummary(props) {
+  const events = props.events;
+  const now = moment();
+  const currentEvent = events.find(
+    event => now.isAfter(event.start) && now.isBefore(event.end),
+  );
+  const futureEvents = events.filter(event => now.isBefore(event.start)).sort();
+  const nextEvent = futureEvents && futureEvents[0];
+  const numEventsToday = futureEvents.filter(event =>
+    now.isSame(event.start, 'day'),
+  ).length;
+  const numEventsThisWeek = futureEvents.filter(event =>
+    now.isSame(event.start, 'week'),
+  ).length;
+  const numEventsThisMonth = futureEvents.filter(event =>
+    now.isSame(event.start, 'month'),
+  ).length;
 
-    return (
-      <div className={styles.calendarSummary}>
-        <h1>Your Scheduled Adventures</h1>
+  return (
+    <div className={styles.calendarSummary}>
+      <h1>Your Scheduled Adventures</h1>
 
-        <h3>Current:</h3>
-        <ActivityCard
-          activity={currentEvent}
-          messageIfNoEvent='Nothing on right now'
-          onMap={false}
-        />
+      <h3>Current:</h3>
+      <ActivityCard
+        activity={currentEvent}
+        messageIfNoEvent='Nothing on right now'
+        onMap={false}
+      />
 
-        <h3>Next:</h3>
-        <ActivityCard activity={nextEvent} onMap={false} />
+      <h3>Next:</h3>
+      <ActivityCard activity={nextEvent} onMap={false} />
 
-        {numEventsThisMonth > 0 && (
-          <div>
-            <h3>Coming up later...</h3>
-            <p>
-              <strong>Later today:</strong> {numEventsToday}
-              <br />
-              <strong>Later this week:</strong> {numEventsThisWeek}
-              <br />
-              <strong>Later this month:</strong> {numEventsThisMonth}
-            </p>
-          </div>
-        )}
-      </div>
-    );
-  }
+      {numEventsThisMonth > 0 && (
+        <div>
+          <h3>Coming up later...</h3>
+          <p>
+            <strong>Later today:</strong> {numEventsToday}
+            <br />
+            <strong>Later this week:</strong> {numEventsThisWeek}
+            <br />
+            <strong>Later this month:</strong> {numEventsThisMonth}
+          </p>
+        </div>
+      )}
+    </div>
+  );
 }
 
 CalendarSummary.propTypes = {
