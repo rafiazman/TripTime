249193896c3f07485ad6fb4ebe2e2d4f1dd355fe
Rafi Azman <r_a@posteo.nz>
2020-05-13 12:21:11 +1200

    Make NoteFactory use existing Travel or Activity within DB instead of creating a new one

--- src/api/database/factories/NoteFactory.php
+++ src/api/database/factories/NoteFactory.php
@@ -2,22 +2,40 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use App\Activity;
 use App\Note;
+use App\Travel;
+use App\User;
 use Faker\Generator as Faker;
 
 $factory->define(Note::class, function (Faker $faker) {
     $pointers = [
-        \App\Activity::class,
-        \App\Travel::class,
+        Activity::class,
+        Travel::class,
     ];
 
     $pointerType = $faker->randomElement($pointers);
-    $pointer = factory($pointerType)->create();
+    $pointerTypeCount = $pointerType::all()->count();
+
+    // If it doesnt exist, randomly choose between Activity and Travel
+    // until found one that already exists in DB
+    $attempts = 0;
+    while ($pointerTypeCount < 1) {
+        if ($attempts == 500) throw new Exception('Could not find Travel or Activity within DB after 500 attempts');
+
+        $pointerType = $faker->randomElement($pointers);
+        $pointerTypeCount = $pointerType::all()->count();
+
+        $attempts++;
+    }
+
+    $pointerIds = $pointerType::pluck('id')->toArray();
 
     return [
         'body' => $faker->text,
-        'user_id' => \App\User::all()->random()->id,
-        'pointer_id' => $pointer->id,
+        'user_id' => User::all()->random()->id,
+        'pointer_id' => $faker->unique()->randomElement($pointerIds),
         'pointer_type' => $pointerType,
     ];
 });
+
