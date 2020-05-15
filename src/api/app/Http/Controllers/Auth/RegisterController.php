3a8a46f279349b34005ca8f75a5439a9d502fce7
Rafi Azman <rafi.azman.7@gmail.com>
2020-05-15 22:41:16 +1200

    Use a more fun randomly generated avatar by default

--- src/api/app/Http/Controllers/Auth/RegisterController.php
+++ src/api/app/Http/Controllers/Auth/RegisterController.php
@@ -64,10 +64,13 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        $email = data['email'];
+
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'avatar_url' => "https://api.adorable.io/avatars/285/$email.png"
         ]);
     }
 }
--- src/api/database/factories/UserFactory.php
+++ src/api/database/factories/UserFactory.php
@@ -18,12 +18,13 @@ use Illuminate\Support\Str;
 */
 
 $factory->define(User::class, function (Faker $faker) {
+    $email = $faker->unique()->safeEmail;
     return [
         'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
+        'email' => $email,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
-        'avatar_url' => 'http://via.placeholder.com/150x150',
+        'avatar_url' => "https://api.adorable.io/avatars/285/$email.png",
     ];
 });
