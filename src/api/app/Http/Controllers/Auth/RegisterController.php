194b36dcc62a8f45c1019330f6f81a46ada4851e
Rafi Azman <rafi.azman.7@gmail.com>
2020-05-15 22:49:50 +1200

    Fix breaking test

--- src/api/app/Http/Controllers/Auth/RegisterController.php
+++ src/api/app/Http/Controllers/Auth/RegisterController.php
@@ -64,7 +64,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        $email = data['email'];
+        $email = $data['email'];
 
         return User::create([
             'name' => $data['name'],
