f3fff33a3715f36c9c4c4b86d33fef607071a33c
niuniuanran <irene.anran.niu@gmail.com>
2020-05-18 00:15:45 +1200

    Use TripContext for timeline components

--- src/spa/components/timeline/TripTimeline.js
+++ src/spa/components/timeline/TripTimeline.js
@@ -3,27 +3,14 @@
 import React from 'react';
 import styles from '../../css/timeline.module.css';
 import ActivityCard from '../cards/ActivityCard';
-import PropTypes from 'prop-types';
 import TravelCard from '../cards/TravelCard';
-import axios from 'axios';
 import ReactLoading from 'react-loading';
 import MomentUtils from '@date-io/moment';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import { TripContext } from '../../contexts/TripContext';
 
-export default class TripTimeline extends React.Component {
-  _isMounted = false;
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      activities: [],
-      travels: [],
-      activityLoading: true,
-      travelLoading: true,
-    };
-  }
-
-  populateTimeline(activities, travels) {
+export default function TripTimeline() {
+  const populateTimeline = (activities, travels, tripID) => {
     const events = [...activities, ...travels].sort((a, b) =>
       a.start.localeCompare(b.start),
     );
@@ -33,7 +20,7 @@ export default class TripTimeline extends React.Component {
         return (
           <div key={index} className={styles.travelCardContainer}>
             <div className={styles.travelCard}>
-              <TravelCard travel={event} tripId={this.props.tripID} />
+              <TravelCard travel={event} tripId={tripID} />
             </div>
           </div>
         );
@@ -48,71 +35,47 @@ export default class TripTimeline extends React.Component {
               onMap={false}
               activity={event}
               onClose={null}
-              tripId={this.props.tripID}
+              tripId={tripID}
             />
           </div>
         );
       }
     });
-  }
+  };
 
-  componentDidMount() {
-    this._isMounted = true;
-
-    const hostName = process.env.API_HOSTNAME;
-    const tripID = this.props.tripID;
-    axios.defaults.withCredentials = true;
-    axios
-      .get(`${hostName}/api/trip/${tripID}/activities`)
-      .then(
-        res => this.setState(() => ({ activities: res.data })),
-        () => this.setState(() => ({ activities: [] })),
-      )
-      .then(() => {
-        if (this._isMounted) this.setState(() => ({ activityLoading: false }));
-      });
-
-    axios
-      .get(`${hostName}/api/trip/${tripID}/travels`)
-      .then(
-        res => this.setState(() => ({ travels: res.data })),
-        () => this.setState(() => ({ travels: [] })),
-      )
-      .then(() => {
-        if (this._isMounted) this.setState(() => ({ travelLoading: false }));
-      });
-  }
-  componentWillUnmount() {
-    this._isMounted = false;
-  }
-
-  render() {
-    return (
-      <div className={styles.tripTimelineContainer}>
-        <div className={styles.tripTimeline}>
-          {this.state.activityLoading || this.state.travelLoading ? (
-            <div className={styles.timelineLoading}>
-              <ReactLoading type='spinningBubbles' color='#ff4200' />
-              <p> Loading the timeline...</p>
-            </div>
-          ) : (
-            <MuiPickersUtilsProvider utils={MomentUtils}>
-              {this.populateTimeline(
-                this.state.activities,
-                this.state.travels.map(travel => {
-                  travel.type = 'travel';
-                  return travel;
-                }),
-              )}
-            </MuiPickersUtilsProvider>
-          )}
-          <div className={styles.line} />
+  return (
+    <TripContext.Consumer>
+      {({
+        activities,
+        activitiesLoading,
+        travels,
+        travelsLoading,
+        trip,
+        tripLoading,
+      }) => (
+        <div className={styles.tripTimelineContainer}>
+          <div className={styles.tripTimeline}>
+            {activitiesLoading || travelsLoading || tripLoading ? (
+              <div className={styles.timelineLoading}>
+                <ReactLoading type='spinningBubbles' color='#ff4200' />
+                <p> Loading the timeline...</p>
+              </div>
+            ) : (
+              <MuiPickersUtilsProvider utils={MomentUtils}>
+                {populateTimeline(
+                  activities,
+                  travels.map(travel => {
+                    travel.type = 'travel';
+                    return travel;
+                  }),
+                  trip.id,
+                )}
+              </MuiPickersUtilsProvider>
+            )}
+            <div className={styles.line} />
+          </div>
         </div>
-      </div>
-    );
-  }
+      )}
+    </TripContext.Consumer>
+  );
 }
-
-TripTimeline.propTypes = {
-  tripID: PropTypes.string,
-};
--- src/spa/components/timeline/TripTitle.js
+++ src/spa/components/timeline/TripTitle.js
@@ -2,50 +2,51 @@
 
 import React, { useState } from 'react';
 import styles from '../../css/trip-summary.module.css';
-import PropTypes from 'prop-types';
 import PeopleList from '../people/PeopleList';
 import AddPeopleDialog from '../people/AddPeopleDialog';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import Tooltip from '../Tooltip';
+import { TripContext } from '../../contexts/TripContext';
 
-export default function TripTitle(props) {
+export default function TripTitle() {
   const [adding, setAdding] = useState(false);
   return (
-    <div className={styles.tripTitleContainer}>
-      <h1 className={styles.tripName}>{props.name}</h1>
-      <p>{props.description}</p>
-      <PeopleList
-        people={props.people}
-        addComponent={
-          <>
-            <Tooltip
-              component={
-                <div
-                  className={styles.addButton}
-                  onClick={() => setAdding(true)}
-                >
-                  <FontAwesomeIcon icon={faPlus} />
-                </div>
-              }
-              text={'add new participant'}
-            />
-            {adding && (
-              <AddPeopleDialog
-                tripID={props.id}
-                onCancel={() => setAdding(false)}
+    <TripContext.Consumer>
+      {({ tripLoading, trip }) => {
+        return (
+          !tripLoading && (
+            <div className={styles.tripTitleContainer}>
+              <h1 className={styles.tripName}>{trip.name}</h1>
+              <p>{trip.description}</p>
+              <PeopleList
+                people={trip.participants}
+                addComponent={
+                  <>
+                    <Tooltip
+                      component={
+                        <div
+                          className={styles.addButton}
+                          onClick={() => setAdding(true)}
+                        >
+                          <FontAwesomeIcon icon={faPlus} />
+                        </div>
+                      }
+                      text={'add new participant'}
+                    />
+                    {adding && (
+                      <AddPeopleDialog
+                        tripID={trip.id}
+                        onCancel={() => setAdding(false)}
+                      />
+                    )}
+                  </>
+                }
               />
-            )}
-          </>
-        }
-      />
-    </div>
+            </div>
+          )
+        );
+      }}
+    </TripContext.Consumer>
   );
 }
-
-TripTitle.propTypes = {
-  name: PropTypes.string.isRequired,
-  people: PropTypes.array.isRequired,
-  description: PropTypes.string,
-  id: PropTypes.string.isRequired,
-};
