8f48dca75b2023a81f311f03b7883a86688c3050
Rafi Azman <rafi.azman.7@gmail.com>
2020-05-22 11:37:02 +1200

    Add JSDoc documentation to custom axios

--- src/spa/app/axios.js
+++ src/spa/app/axios.js
@@ -2,6 +2,11 @@
 
 import axios from 'axios';
 
+/**
+ * Creates an instance of axios pre-configured with settings
+ * to interact with backend SPA authentication system
+ * @returns {axios}
+ */
 export default axios.create({
   baseURL: `${process.env.API_HOSTNAME}/api`,
   withCredentials: true,
