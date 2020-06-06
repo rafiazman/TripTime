95062ee3e7889da9df3efa2495019f33c98faa04
Rafi Azman <rafi.azman.7@gmail.com>
2020-06-06 21:58:35 +1200

    Fix avatar URL not being generated on register

--- src/api/app/User.php
+++ src/api/app/User.php
@@ -52,7 +52,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'avatar_url',
     ];
 
     /**
